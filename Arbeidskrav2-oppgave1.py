b9835bf12350bfc459ef0d841c767852ce13a8f1
larsknutsen <lars@larsknutsen.com>
2025-01-14 14:16:45 +0100

    Update Arbeidskrav2-oppgave1.py

--- Arbeidskrav2-oppgave1.py
+++ Arbeidskrav2-oppgave1.py
@@ -5,10 +5,15 @@ Created on Sun Jan 12 11:40:32 2025
 @author: lars
 """
 
+from datetime import datetime
+
 clear5 = "\n" * 5 #legger 5 linjeskift til variabel clear5
 print (clear5) #printer variabel clear5 (linjeskiftene) for å gjøre konsollet mer lesbart 
 
-alder = int(input("Hvilket år er du født?"))
-gammel = 2025-alder
+dagens_ar = datetime.now().year #henter dagens årstall
+alder = int(input("Hvilket år er du født? "))
+gammel = dagens_ar-alder #regner ut alder i antall år
 
 print("Du er", gammel, "år gammel")
+
+
